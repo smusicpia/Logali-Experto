19d8dab1a449295bb2896df22cc20e94fe3e5883
smusicpia <s_musicpia@yahoo.com.mx>
2024-03-13 17:13:45 +0000

    First Commit to Proyect_Fiori

---
+++ Employees/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("logaligroup.employees.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
