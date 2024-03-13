0d24061626c846a153293a3e20afeadfcf968d9a
smusicpia <s_musicpia@yahoo.com.mx>
2024-03-13 23:56:39 +0000

    First Commit 04

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -7,9 +7,23 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("logaligroup.employees.controller.MainView", {
-            onInit: function () {
+        function myCheck() {
+            var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+                if (valueEmployee.length === 6){
+                    //inputEmployee.setDescription("OK");
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("slCountry").setVisible(true);
+                } else {
+                    //inputEmployee.setDescription("Not OK");
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("slCountry").setVisible(false);
+                }
+        }
 
-            }
-        });
+        const Main = Controller.extend("logaligroup.employees.controller.MainView", {});
+
+        Main.prototype.onValidate = myCheck
+
+        return Main;
     });
